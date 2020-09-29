8da924058165309c499f5a962e9a5e001fa4812c
unknown <onediniz2@gmail.com>
2020-09-29 01:28:16 -0300

    ExCurso

---
+++ Ex_Produtos_Estoque/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+namespace Ex_Produtos_Estoque
+{
+    class Produto
+    {   
+        //ATRIBUTOS DA CLASSE Produto
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+       
+        //CONTABILIZA O ESTOQUE
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+
+        }
+
+        //ADICIONA PRODUTOS AO ESTOQUE
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+
+        }
+        
+        //REMOVE PRODUTOS DO ESTOQUE
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade -= quantidade; 
+        }
+
+
+
+       
+        //SOBREPOSIÇÃO OVERRIDE.ToString    TRANSFORMA UM PRODUTO EM STRING. QUANDO O OBJETO É COLOCADO DENTRO DE UM CW , ELE CHAMA O MÉTODO ToString JA COM A FORMATAÇÃO.
+        public override string ToString()
+        {
+            return Nome + ", $" + Preco.ToString("F2",CultureInfo.InvariantCulture) 
+                + ", " + Quantidade + " unidade, Total: $ "  
+                +ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture); 
+        }
+    }
+}
---
+++ Ex_Produtos_Estoque/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex_Produtos_Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.WriteLine("NOME");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("PREÇO");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("QUANTIDADE NO ESTOQUE");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto" + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque:");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("Dados Atualizados: "+ p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque:");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+
+
+
+
+
+            
+            
+
+
+
+
+        }
+    }
+}
